6f0765027560cc09a75d24f41cda229449689fb6
Ben <ba.lowbridge@gmail.com>
2017-10-27 11:52:39 +1000

    removing semi-colons

--- recorder.js
+++ recorder.js
@@ -1,187 +1,187 @@
 'use strict'
 
 export default function (containerID, minutes, seconds) {
-	let log = console.log.bind(console)
-	this.container = document.getElementById(containerID)
-	this.minutes = minutes
-	this.seconds = seconds
-	this.start = document.getElementById('start')
-	this.stop = document.getElementById('stop')
-	this.counter = 0
-	this.startButton = `<button type="button" id="start">Start</button>`
-	this.stopButton = `<button type="button" id="stop">Stop</button>`
-	this.countdownTimer = `<div id="countdownTimer"></div>`
-	this.mediaOptions = {
+    let log = console.log.bind(console)
+    this.container = document.getElementById(containerID)
+    this.minutes = minutes
+    this.seconds = seconds
+    this.start = document.getElementById('start')
+    this.stop = document.getElementById('stop')
+    this.counter = 0
+    this.startButton = `<button type="button" id="start">Start</button>`
+    this.stopButton = `<button type="button" id="stop">Stop</button>`
+    this.countdownTimer = `<div id="countdownTimer"></div>`
+    this.mediaOptions = {
         video: {
-          tag: 'video',
-          type: 'video/webm',
-          ext: '.mp4',
-          gUM: {video: true, audio: true}
+            tag: 'video',
+            type: 'video/webm',
+            ext: '.mp4',
+            gUM: { video: true, audio: true }
         },
         audio: {
-          tag: 'audio',
-          type: 'audio/mp3',
-          ext: '.mp3',
-          gUM: {audio: true}
+            tag: 'audio',
+            type: 'audio/mp3',
+            ext: '.mp3',
+            gUM: { audio: true }
         }
-      }
-	this.mediaType = this.mediaOptions.audio
-	this.stream = null
-	this.recorder = null
-	this.chunks = null
+    }
+    this.mediaType = this.mediaOptions.audio
+    this.stream = null
+    this.recorder = null
+    this.chunks = null
 
-	this.container.insertAdjacentHTML('beforeend', this.startButton)
-	this.container.insertAdjacentHTML('beforeend', this.stopButton)
-	this.container.insertAdjacentHTML('beforeend', this.countdownTimer)
+    this.container.insertAdjacentHTML('beforeend', this.startButton)
+    this.container.insertAdjacentHTML('beforeend', this.stopButton)
+    this.container.insertAdjacentHTML('beforeend', this.countdownTimer)
 
-	this.initialiseMedia = function () {
-		navigator.mediaDevices.getUserMedia(this.mediaType.gUM).then(_stream => {
-		    this.stream = _stream
-		    document.getElementById('start').disabled = false
-		    this.recorder = new MediaRecorder(this.stream)
-		    this.recorder.ondataavailable = e => {
-			    this.chunks.push(e.data)
-			    if(this.recorder.state == 'inactive')  this.makeLink()
-		    }
-		    log('got media successfully')
-		  }).catch(log)
+    this.initialiseMedia = function () {
+        navigator.mediaDevices.getUserMedia(this.mediaType.gUM).then(_stream => {
+            this.stream = _stream
+            document.getElementById('start').disabled = false
+            this.recorder = new MediaRecorder(this.stream)
+            this.recorder.ondataavailable = e => {
+                this.chunks.push(e.data)
+                if (this.recorder.state == 'inactive') this.makeLink()
+            }
+            log('got media successfully')
+        }).catch(log)
 
-		document.getElementById('start').addEventListener('click', ()=> {
-			this.startRecording()
-		})
+        document.getElementById('start').addEventListener('click', () => {
+            this.startRecording()
+        })
 
-		document.getElementById('stop').addEventListener('click', ()=> {
-			this.stopRecording()
-		})
-	}
-	
-	/**
-	 * Changes default media type
-	 */
-	this.setMediaType = function (type) {
-		if (type == 'video') {
-			this.mediaType = this.mediaOptions.video
-		} else {
-			this.mediaType = this.mediaOptions.audio
-		}
-	}
+        document.getElementById('stop').addEventListener('click', () => {
+            this.stopRecording()
+        })
+    }
 
-	/**
-	 * Toggles Media Type
-	 */
+    /**
+     * Changes default media type
+     */
+    this.setMediaType = function (type) {
+        if (type == 'video') {
+            this.mediaType = this.mediaOptions.video
+        } else {
+            this.mediaType = this.mediaOptions.audio
+        }
+    }
 
-	this.toggleMediaType = function () {
-		if (this.mediaType.tag == 'audio') {
-			this.mediaType = this.mediaOptions.video
-		} else {
-			this.mediaType = this.mediaOptions.audio
-		}	
-	}
+    /**
+     * Toggles Media Type
+     */
 
-	/**
-	 * Returns current Media type
-	 */
-	this.getMediaType = function() {
-	 	return this.mediaType.tag
-	}
+    this.toggleMediaType = function () {
+        if (this.mediaType.tag == 'audio') {
+            this.mediaType = this.mediaOptions.video
+        } else {
+            this.mediaType = this.mediaOptions.audio
+        }
+    }
 
-	 /**
-	  * Starts recording
-	  */
-	 this.startRecording = function () {
-	    document.getElementById('start').disabled = true
-	    document.getElementById('stop').disabled = false
-	    this.chunks=[]
-	    this.recorder.start()
-	    countdown('countdownTimer', this.minutes, this.seconds)
-	}
+    /**
+     * Returns current Media type
+     */
+    this.getMediaType = function () {
+        return this.mediaType.tag
+    }
 
-	  /**
-	   * Stops recording
-	   */
-	this.stopRecording = function () {
-		document.getElementById('stop').disabled = true
-		this.recorder.stop()
-		document.getElementById('start').disabled = false
-		document.getElementById('countdownTimer').innerHTML = ''
-	}
+    /**
+     * Starts recording
+    */
+    this.startRecording = function () {
+        document.getElementById('start').disabled = true
+        document.getElementById('stop').disabled = false
+        this.chunks = []
+        this.recorder.start()
+        countdown('countdownTimer', this.minutes, this.seconds)
+    }
 
-	  /**
-	   * generates the download button
-	   */
+    /**
+     * Stops recording
+     */
+    this.stopRecording = function () {
+        document.getElementById('stop').disabled = true
+        this.recorder.stop()
+        document.getElementById('start').disabled = false
+        document.getElementById('countdownTimer').innerHTML = ''
+    }
 
-	  this.makeLink = function () {
-		  let blob = new Blob(this.chunks, {type: this.mediaType.type })
-		    , url = URL.createObjectURL(blob)
-		    , li = document.createElement('div')
-		    , mt = document.createElement(this.mediaType.tag)
-		    , bt = document.createElement('button')
-		    , hf = document.createElement('a')
-		    , dl = document.createElement('button')
-			this.counter++
-		  mt.controls = true
-		  mt.src = url
-		  mt.id = 'media-file'
+    /**
+     * generates the download button
+     */
 
-		  hf.href = url
-		  hf.download = `${this.guid()}`
+    this.makeLink = function () {
+        let blob = new Blob(this.chunks, { type: this.mediaType.type })
+            , url = URL.createObjectURL(blob)
+            , li = document.createElement('div')
+            , mt = document.createElement(this.mediaType.tag)
+            , bt = document.createElement('button')
+            , hf = document.createElement('a')
+            , dl = document.createElement('button')
+        this.counter++
+        mt.controls = true
+        mt.src = url
+        mt.id = 'media-file'
 
-		  li.id = `${hf.download}`
+        hf.href = url
+        hf.download = `${this.guid()}`
 
-		  bt.innerHTML = `download ${hf.download}${this.mediaType.ext}`
-		  hf.id = 'download-media-file'
-		  bt.classList.add('btn')
+        li.id = `${hf.download}`
 
-		  dl.innerHTML = `delete media`
-		  dl.id = `delete-${this.counter}`
-		  dl.classList.add('btn')
-		  dl.href = '#'
+        bt.innerHTML = `download ${hf.download}${this.mediaType.ext}`
+        hf.id = 'download-media-file'
+        bt.classList.add('btn')
 
-		  hf.appendChild(bt)
-		  li.appendChild(mt)
-		  li.appendChild(hf)
-		  li.appendChild(dl)
-		  this.container.appendChild(li)
+        dl.innerHTML = `delete media`
+        dl.id = `delete-${this.counter}`
+        dl.classList.add('btn')
+        dl.href = '#'
 
-		  document.getElementById(`delete-${this.counter}`).addEventListener('click', ()=> {
-		  	document.getElementById(hf.download).outerHTML = ''
-		  })
-	  }
+        hf.appendChild(bt)
+        li.appendChild(mt)
+        li.appendChild(hf)
+        li.appendChild(dl)
+        this.container.appendChild(li)
 
-	  /**
-	   * Generates a unique filename
-	   */
+        document.getElementById(`delete-${this.counter}`).addEventListener('click', () => {
+            document.getElementById(hf.download).outerHTML = ''
+        })
+    }
 
-	  this.guid = function () {
-	  	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-	    s4() + '-' + s4() + s4() + s4()
-	  }
+    /**
+     * Generates a unique filename
+     */
 
-	  function s4() {
-		  return Math.floor((1 + Math.random()) * 0x10000)
-		    .toString(16)
-		    .substring(1)
-		}
+    this.guid = function () {
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4()
+    }
+
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1)
+    }
 
 
-	function countdown(element, minutes, seconds) {
-	    // set time for the particular countdown
-	    var time = minutes*60 + seconds;
-	    var interval = setInterval(function() {
-	        var el = document.getElementById(element);
-	        // if the time is 0 then end the counter
-	        if (time <= 0) {
-	            document.getElementById('stop').click()
-	            clearInterval(interval);
-	            return;
-	        }
-	        var minutes = Math.floor( time / 60 );
-	        if (minutes < 10) minutes = "0" + minutes;
-	        var seconds = time % 60;
-	        if (seconds < 10) seconds = "0" + seconds; 
-	        var text = minutes + ':' + seconds;
-	        el.innerHTML = text;
-	        time--;
-	    }, 1000);
-	}
+    function countdown(element, minutes, seconds) {
+        // set time for the particular countdown
+        var time = minutes * 60 + seconds
+        var interval = setInterval(function () {
+            var el = document.getElementById(element)
+            // if the time is 0 then end the counter
+            if (time <= 0) {
+                document.getElementById('stop').click()
+                clearInterval(interval)
+                return
+            }
+            var minutes = Math.floor(time / 60)
+            if (minutes < 10) minutes = "0" + minutes
+            var seconds = time % 60
+            if (seconds < 10) seconds = "0" + seconds
+            var text = minutes + ':' + seconds
+            el.innerHTML = text
+            time--
+        }, 1000)
+    }
 }
